746e87a698a3d0efb85bb64a3b988ea96a27a4b9
Sumit Sijher <sumitsijher@gmail.com>
2020-06-27 00:30:59 -0700

    added devicePixelRatio scaling

--- script.js
+++ script.js
@@ -36,6 +36,8 @@ var H;
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+var scale = window.devicePixelRatio;
+ctx.scale(scale, scale);
 
 startAnimating(fps);
 
